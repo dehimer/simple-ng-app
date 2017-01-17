8014c2324e32c743006389225264cbc5ff1ceaaa
Denis Novikov (dehimer) <den-vse@yandex.ru>
2017-01-17 03:23:43 +0300

    add components of books and rooms

---
+++ app/books.component.ts
@@ -0,0 +1,7 @@
+import { Component} from '@angular/core';
+  
+@Component({
+    selector: 'books',
+    template: `<h3>Книги</h3>`
+})
+export class BooksComponent { }
---
+++ app/rooms.component.ts
@@ -0,0 +1,7 @@
+import { Component} from '@angular/core';
+  
+@Component({
+    selector: 'rooms',
+    template: `<h3>Комнаты</h3>`
+})
+export class RoomsComponent { }
